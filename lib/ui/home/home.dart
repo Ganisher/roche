134239d5217aa684d2abbf569d6eccee3aa0d25c
G'anisher <git@ganisher.kz>
2021-04-24 15:24:25 +0600

    feat: added routes class, class for home page.

---
+++ lib/ui/home/home.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('Hello, World'),
+          ],
+        ),
+      ),
+    );
+  }
+}
